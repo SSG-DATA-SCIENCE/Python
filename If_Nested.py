188cc17d96159b90b305ada5fc3490a86c1d22e8
Sarabjit singh gambhir <sarabjit.singh.gnmba26@iilm.edu>
2025-06-11 11:44:08 +0530

    Add files via upload

---
+++ If_Nested.py
@@ -0,0 +1,10 @@
+name ="Sarabjit"
+age=24
+if name=="Sarabjit":
+    if age== 24:
+        print("The Data Provided is Correct")
+    else:
+        print("Name is correct but age is incorrect")
+else:
+    print("Both Name and Age is Incorrect")      
+
